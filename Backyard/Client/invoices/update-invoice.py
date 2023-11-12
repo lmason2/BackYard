c34a6b6b86adf502a38ed54d3ad702fdc116fdd9
Luke Mason <lukesamuelmason@gmail.com>
2023-11-12 15:58:07 -0700

    finished

--- Backyard/Client/invoices/update-invoice.py
+++ Backyard/Client/invoices/update-invoice.py
@@ -1,3 +1,31 @@
 '''
 /update-invoice
 '''
+
+import requests
+import base64
+import json
+from datetime import datetime, timedelta
+
+BASE_URL = 'http://127.0.0.1:8000/api/backyard/v1/update-invoice'
+
+HEADERS = {
+
+}
+
+now = datetime.now()
+time_change = timedelta(days=15) 
+update = now + time_change
+
+PAYLOAD = {
+    'invoice_id': 'e72ac7a2-d511-419b-b0b1-98a1a24cb359',
+    'values': json.dumps({
+        'due_date': update.strftime('%Y-%m-%d'),
+    })
+}
+
+encoded_payload = base64.b64encode(json.dumps(PAYLOAD).encode())
+
+response = requests.post(BASE_URL, data=encoded_payload, headers=HEADERS)
+
+print(response.text)
